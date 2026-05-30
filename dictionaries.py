4241138266fa44811782886eb29e7c083307c889
herdevera <jay.business_01@yahoo.com>
2026-05-30 23:38:11 +0800

    dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -11,3 +11,10 @@ print(person["name"])
 print(person["job"])
 person ["hobby"] = "Sleeping"
 print(person)
+
+print(person.keys())        #think of it like a column or field in excel
+print(person.values())      # row data
+print(person.items())       # both column name and data
+
+for key, value in person.items ():
+    print(f"{key}: {value}")
