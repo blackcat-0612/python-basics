66dea2879f40a8e8d50f08ce0803526827c36c7a
herdevera <jay.business_01@yahoo.com>
2026-05-30 23:27:21 +0800

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+# dictionaries
+person = {"name": "Alicia", "age": 31, "city": "Shanghai","job": "Tech BA","language": "SQL, Python"}
+print(person["name"])
+print(person["age"])
+print(person["city"])
+print(person["job"])
+print(person["language"])
+
+print(person)
+print(person["name"])
+print(person["job"])
+person ["hobby"] = "Sleeping"
+print(person)
