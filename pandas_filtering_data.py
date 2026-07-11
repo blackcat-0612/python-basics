8f81c2e5d68b63384fcc7e0cfc1cf7f59ab26e8a
herdevera <jay.business_01@yahoo.com>
2026-07-11 23:09:46 +0800

    pandas_filtering.py

--- pandas_filtering_data.py
+++ pandas_filtering_data.py
@@ -4,7 +4,8 @@ import pandas as pd
 data = {
     "name": ["Jessica", "Alice", "Tony", "Paula"],
     "age": [28, 23, 56, 31],
-    "salary": [60000, 78000, 567000, 678001]
+    "salary": [60000, 78000, 567000, 678001],
+    "department": ["HR", "IT", "IT", "HR"]
 }
 
 df = pd.DataFrame(data)
@@ -15,5 +16,11 @@ print(df[df["salary"] >= 80000])
 
 
 print(df[df["name"] == "Jessica"])
+
+print(df.groupby("department")["salary"].sum())
+
+print(df.groupby("department")["salary"].min())
+
+print(df.groupby("department")["age"].max())
 
 
